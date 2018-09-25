bef119ecdd816055400c34bd290c525cea59ebee
jeff oriente <jdanieloriente@gmail.com>
2018-09-25 12:14:42 -0400

    Did Average Drill Now

--- drill1.js
+++ drill1.js
@@ -32,3 +32,23 @@ function min(numbers){
 let myArr = [0 ,1, 2, 3, 4, 5, -15];
 console.log(max(myArr));
 console.log(min(myArr));
+
+/////////////////////////////////////////////////////////////
+function average(numbers) {
+  let total = 0;
+  numbers.forEach(function(number) {
+    total += number;
+  })
+  return total/numbers.length;
+}
+
+console.log(average([5, 6, 7, 8, 9]));
+
+/* From here down, you are not expected to 
+   understand.... for now :)  
+   
+   
+   Nothing to see here!
+   
+*/
+
