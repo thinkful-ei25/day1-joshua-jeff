9c3b1fcb6f20595fceee72b35e40fafaaf8da98d
jeff oriente <jdanieloriente@gmail.com>
2018-09-25 15:07:20 -0400

    Added Closures now

--- higherFunctions.js
+++ higherFunctions.js
@@ -57,3 +57,44 @@ console.log(filteredNames) // => ['Rich', 'Ray']
 // <---- DO NOT EDIT BETWEEN THESE LINES
 
 console.log(myNames);
+
+
+////////////////////////////////////////////////////////////
+function hazardWarningCreator(typeOfWarning) {
+	let warningCounter = 0;
+	return function(location) {
+		warningCounter++;
+		console.log(`DANGER!  There is a ${typeOfWarning} hazard at ${location}!`);
+		console.log(`The ${typeOfWarning} hazard alert has triggered ${warningCounter} time(s) today!`);
+	}
+}
+
+const rocksWarning = hazardWarningCreator('Rocks on the Road');
+const floodsWarning = hazardWarningCreator('Floods on the Road');
+const volcanoWarning = hazardWarningCreator('Volcano on the Road');
+
+rocksWarning('Main St and Pacific Ave');
+rocksWarning('Main St and South Ave');
+rocksWarning('Main St and Central Ave');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
