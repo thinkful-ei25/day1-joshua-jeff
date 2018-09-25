c9b96bd87ae6af98f356b90fa4f03f30f6697ebd
jeff oriente <jdanieloriente@gmail.com>
2018-09-25 18:54:33 -0400

    Worked on reduce more

--- higherFunctions.js
+++ higherFunctions.js
@@ -117,10 +117,34 @@ console.log(turtleFilterFun(turtleMove));
 console.log(turtleMap(turtleMove));
 console.log(turtleEach(turtleMove2));
 
+//////////////////////////REDUCE PROBLEM /////////////////////////////////
+// reduce
+// Use the reduce function to iterate through an array of words and construct a decoded sentence (string) based on the following criteria:
+// If the array element is exactly three characters in length, add a space character to your accumulator
+// Otherwise, capitalize the LAST character of the array element and add it to your accumulator
+// Your input is 'noggin oreo the moon time tele steed his tent apollo her lives though shoo tofu budapest'
+// You will need to convert the input to an array before using .reduce()
+// HINT: When you invoke reduce() you will need to set the initialValue parameter to an empty string so that future iterations can concatenate more string characters
+
+function reducer(sentence) {
+let splittedArray = sentence.split(' ');
+let newWord = splittedArray.reduce(function(initialValue, currentValue){
+	if(splittedArray(currentValue).length === 3) {
+		initialValue += ' ';
+		return initialValue;
+	}
+	else {
+		initialValue += splittedArray(currentValue).charAt(currentValue.length-1).toUpperCase();
+		return initialValue;
+	}
+	}, '');
+return newWord;
+};
 
 
 
-
+let input = 'noggin oreo the moon time tele steed his tent apollo her lives though shoo tofu budapest';
+console.log(reducer(input));
 
 
 
